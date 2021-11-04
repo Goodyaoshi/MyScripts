ac1f7e99ffb39e553c3b89c019eb70117cb96f29
Goodyaoshi <liyaoshi03328@qq.com>
2021-11-05 04:31:00 +0800

    2021-11-05 04:31:00

--- Shell/jdScripts.sh
+++ Shell/jdScripts.sh
@@ -1,149 +1,149 @@
 #!/bin/bash
-cd /root/JDScripts/
-node /root/JDScripts/cleancart_activity.js
-node /root/JDScripts/jd_88hb.js
-node /root/JDScripts/jd_IndustryLottery.js
-node /root/JDScripts/jd_api_test.js
-node /root/JDScripts/jd_bean_box.js
-node /root/JDScripts/jd_bean_change.js
-node /root/JDScripts/jd_bean_home.js
-node /root/JDScripts/jd_bean_sign.js
-node /root/JDScripts/jd_beauty.js
-node /root/JDScripts/jd_big_winner.js
-node /root/JDScripts/jd_blueCoin.js
-node /root/JDScripts/jd_bookshop.js
-node /root/JDScripts/jd_car.js
-node /root/JDScripts/jd_car_exchange.js
-node /root/JDScripts/jd_carnivalcity.js
-node /root/JDScripts/jd_carnivalcity_help.js
-node /root/JDScripts/jd_cash.js
-node /root/JDScripts/jd_ccSign.js
-node /root/JDScripts/jd_cfd.js
-node /root/JDScripts/jd_cfd_hb.js
-node /root/JDScripts/jd_cfd_loop.js
-node /root/JDScripts/jd_cfd_mooncake.js
-node /root/JDScripts/jd_cfd_shell.js
-node /root/JDScripts/jd_cfd_stock.js
-node /root/JDScripts/jd_checkCookie.js
-node /root/JDScripts/jd_cjhz.js
-node /root/JDScripts/jd_club_lottery.js
-node /root/JDScripts/jd_connoisseur.js
-node /root/JDScripts/jd_daily_egg.js
-node /root/JDScripts/jd_daily_lottery.js
-node /root/JDScripts/jd_ddworld.js
-node /root/JDScripts/jd_dj_bean.js
-node /root/JDScripts/jd_dj_fruit.js
-node /root/JDScripts/jd_dj_fruit_collectWater.js
-node /root/JDScripts/jd_dj_getPoints.js
-node /root/JDScripts/jd_dj_getck.js
-node /root/JDScripts/jd_dj_plantBeans.js
-node /root/JDScripts/jd_dreamFactory.js
-node /root/JDScripts/jd_dreamFactory_help.js
-node /root/JDScripts/jd_dwapp.js
-node /root/JDScripts/jd_esManager.js
-node /root/JDScripts/jd_fcwb.js
-node /root/JDScripts/jd_foodRunning.js
-node /root/JDScripts/jd_forceUpdateCron.js
-node /root/JDScripts/jd_fruit.js
-node /root/JDScripts/jd_fruit_moreTask.js
-node /root/JDScripts/jd_getUp.js
-node /root/JDScripts/jd_gold_creator.js
-node /root/JDScripts/jd_guaMMdou.js
-node /root/JDScripts/jd_guaUnknownTask3.js
-node /root/JDScripts/jd_guacarnivalcity.js
-node /root/JDScripts/jd_guacleancart.js
-node /root/JDScripts/jd_guaddworld.js
-node /root/JDScripts/jd_guaopencard39.js
-node /root/JDScripts/jd_guaopencard49.js
-node /root/JDScripts/jd_guaopencard52.js
-node /root/JDScripts/jd_guaopencard53.js
-node /root/JDScripts/jd_guaopencard54.js
-node /root/JDScripts/jd_guaopencard55.js
-node /root/JDScripts/jd_guaopencard56.js
-node /root/JDScripts/jd_guaopencard57.js
-node /root/JDScripts/jd_guaopencard58.js
-node /root/JDScripts/jd_guaopencard59.js
-node /root/JDScripts/jd_guaopencard60.js
-node /root/JDScripts/jd_guaopencard61.js
-node /root/JDScripts/jd_guaopencard62.js
-node /root/JDScripts/jd_guaopencard63.js
-node /root/JDScripts/jd_guaredEnvelopes.js
-node /root/JDScripts/jd_guawealth_island.js
-node /root/JDScripts/jd_guawealth_island_help.js
-node /root/JDScripts/jd_half_redrain.js
-node /root/JDScripts/jd_health.js
-node /root/JDScripts/jd_health_collect.js
-node /root/JDScripts/jd_jdfactory.js
-node /root/JDScripts/jd_jdzz.js
-node /root/JDScripts/jd_jin_tie.js
-node /root/JDScripts/jd_jingxianghongbao.js
-node /root/JDScripts/jd_joy.js
-node /root/JDScripts/jd_joy_feedPets.js
-node /root/JDScripts/jd_joy_new.js
-node /root/JDScripts/jd_joy_park.js
-node /root/JDScripts/jd_joy_reward.js
-node /root/JDScripts/jd_joy_run.js
-node /root/JDScripts/jd_joy_steal.js
-node /root/JDScripts/jd_jump.js
-node /root/JDScripts/jd_jxg.js
-node /root/JDScripts/jd_jxgc.js
-node /root/JDScripts/jd_jxgc_stock.js
-node /root/JDScripts/jd_jxlhb.js
-node /root/JDScripts/jd_jxmc.js
-node /root/JDScripts/jd_jxmc_getCoin.js
-node /root/JDScripts/jd_jxmc_stock.js
-node /root/JDScripts/jd_jxnc.js
-node /root/JDScripts/jd_jxnnfls.js
-node /root/JDScripts/jd_jxred.js
-node /root/JDScripts/jd_jxsign.js
-node /root/JDScripts/jd_kd.js
-node /root/JDScripts/jd_live.js
-node /root/JDScripts/jd_lol.js
-node /root/JDScripts/jd_lotteryMachine.js
-node /root/JDScripts/jd_market_lottery.js
-node /root/JDScripts/jd_mf.js
-node /root/JDScripts/jd_mf_exchange.js
-node /root/JDScripts/jd_mofang.js
-node /root/JDScripts/jd_mohe.js
-node /root/JDScripts/jd_moneyTree.js
-node /root/JDScripts/jd_ms.js
-node /root/JDScripts/jd_nzmh.js
-node /root/JDScripts/jd_pet.js
-node /root/JDScripts/jd_pigPet.js
-node /root/JDScripts/jd_plantBean.js
-node /root/JDScripts/jd_price.js
-node /root/JDScripts/jd_productZ4Brand.js
-node /root/JDScripts/jd_qjd.js
-node /root/JDScripts/jd_redEnvelope.js
-node /root/JDScripts/jd_redPacket.js
-node /root/JDScripts/jd_reward.js
-node /root/JDScripts/jd_sendBeans.js
-node /root/JDScripts/jd_sgmh.js
-node /root/JDScripts/jd_shop.js
-node /root/JDScripts/jd_shop_sign.js
-node /root/JDScripts/jd_sign_graphics.js
-node /root/JDScripts/jd_sjjc.js
-node /root/JDScripts/jd_small_home.js
-node /root/JDScripts/jd_speed.js
-node /root/JDScripts/jd_speed_redpocke.js
-node /root/JDScripts/jd_speed_sign.js
-node /root/JDScripts/jd_stardreamFactory_tuan.js
-node /root/JDScripts/jd_superBrand.js
-node /root/JDScripts/jd_superMarket.js
-node /root/JDScripts/jd_super_redrain.js
-node /root/JDScripts/jd_syj.js
-node /root/JDScripts/jd_tewu.js
-node /root/JDScripts/jd_track.js
-node /root/JDScripts/jd_try.js
-node /root/JDScripts/jd_ttpt.js
-node /root/JDScripts/jd_unsubscribe.js
-node /root/JDScripts/jd_updatePackage.js
-node /root/JDScripts/jd_validate_Worker.js
-node /root/JDScripts/jd_wish.js
-node /root/JDScripts/jd_xsqjd.js
-node /root/JDScripts/jd_xtg.js
-node /root/JDScripts/jd_yili_cow.js
-node /root/JDScripts/jd_ys.js
-node /root/JDScripts/jd_zsign.js
-node /root/JDScripts/jd_zzt.js
-node /root/JDScripts/sign_graphics_validate.js
+cd/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/cleancart_activity.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_88hb.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_IndustryLottery.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_api_test.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_bean_box.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_bean_change.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_bean_home.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_bean_sign.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_beauty.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_big_winner.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_blueCoin.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_bookshop.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_car.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_car_exchange.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_carnivalcity.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_carnivalcity_help.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_cash.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_ccSign.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_cfd.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_cfd_hb.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_cfd_loop.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_cfd_mooncake.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_cfd_shell.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_cfd_stock.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_checkCookie.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_cjhz.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_club_lottery.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_connoisseur.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_daily_egg.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_daily_lottery.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_ddworld.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_dj_bean.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_dj_fruit.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_dj_fruit_collectWater.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_dj_getPoints.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_dj_getck.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_dj_plantBeans.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_dreamFactory.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_dreamFactory_help.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_dwapp.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_esManager.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_fcwb.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_foodRunning.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_forceUpdateCron.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_fruit.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_fruit_moreTask.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_getUp.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_gold_creator.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaMMdou.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaUnknownTask3.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guacarnivalcity.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guacleancart.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaddworld.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard39.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard49.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard52.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard53.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard54.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard55.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard56.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard57.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard58.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard59.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard60.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard61.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard62.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaopencard63.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guaredEnvelopes.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guawealth_island.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_guawealth_island_help.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_half_redrain.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_health.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_health_collect.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jdfactory.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jdzz.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jin_tie.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jingxianghongbao.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_joy.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_joy_feedPets.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_joy_new.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_joy_park.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_joy_reward.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_joy_run.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_joy_steal.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jump.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxg.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxgc.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxgc_stock.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxlhb.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxmc.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxmc_getCoin.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxmc_stock.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxnc.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxnnfls.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxred.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_jxsign.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_kd.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_live.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_lol.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_lotteryMachine.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_market_lottery.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_mf.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_mf_exchange.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_mofang.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_mohe.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_moneyTree.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_ms.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_nzmh.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_pet.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_pigPet.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_plantBean.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_price.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_productZ4Brand.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_qjd.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_redEnvelope.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_redPacket.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_reward.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_sendBeans.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_sgmh.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_shop.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_shop_sign.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_sign_graphics.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_sjjc.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_small_home.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_speed.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_speed_redpocke.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_speed_sign.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_stardreamFactory_tuan.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_superBrand.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_superMarket.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_super_redrain.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_syj.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_tewu.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_track.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_try.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_ttpt.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_unsubscribe.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_updatePackage.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_validate_Worker.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_wish.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_xsqjd.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_xtg.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_yili_cow.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_ys.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_zsign.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/jd_zzt.js
+node/root/JDScripts/roo/root/JDScripts/JDScript/root/JDScripts/sign_graphics_validate.js
